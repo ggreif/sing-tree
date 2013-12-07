12eea090a4786255c29b8ec74702cb3144e39bba
Gabor Greif <ggreif@gmail.com>
2013-12-07 15:09:09 +0100

    play around with symbol reification

--- Data/SingTree.hs
+++ Data/SingTree.hs
@@ -13,10 +13,16 @@ hey = 1
 data Tree :: [Symbol] -> * where
   Leaf :: Tree p
   Fork :: KnownSymbol n => Proxy n -> Tree p -> Tree (n ': p)
+  Furk :: KnownSymbol n => Tree p -> Tree (n ': p)
 
 deriving instance Show (Tree p)
 
 t1 = Fork (Proxy :: Proxy "dd") Leaf
-t2 = Fork Proxy Leaf :: forall p . Tree ("dd" ': p)
+t2 = Furk Leaf :: forall p . Tree ("dd" ': p)
+
+getNodeName :: Tree (n ': p) -> String
+getNodeName t@(Furk _) = symbolVal (prox t)
+  where prox :: Tree (n ': p) -> Proxy n
+        prox _ = Proxy
 
 
