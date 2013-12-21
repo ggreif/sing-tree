c02ba9b865a5c19ba01ec4ee647d6768025f7c6e
Gabor Greif <ggreif@gmail.com>
2013-12-21 16:45:32 +0100

    rename Furk -> Fork

--- Data/SingTree.hs
+++ Data/SingTree.hs
@@ -10,17 +10,15 @@ import Data.Thrist
 
 data Tree :: [Symbol] -> * where
   Leaf :: Tree p
-  Fork :: KnownSymbol n => Proxy n -> Tree p -> Tree (n ': p)
-  Furk :: KnownSymbol n => Tree p -> Tree (n ': p)
+  Fork :: KnownSymbol n => Tree p -> Tree (n ': p)
 
 deriving instance Show (Tree p)
 
-t1 = Fork (Proxy :: Proxy "dd") Leaf
-t2 = Furk Leaf :: forall p . Tree ("dd" ': p)
-t3 = Furk t2 :: forall p . Tree ("hh" ': "dd" ': p)
+t2 = Fork Leaf :: forall p . Tree ("dd" ': p)
+t3 = Fork t2 :: forall p . Tree ("hh" ': "dd" ': p)
 
 getNodeName :: Tree (n ': p) -> String
-getNodeName t@(Furk _) = symbolVal (prox t)
+getNodeName t@(Fork _) = symbolVal (prox t)
   where prox :: Tree (n ': p) -> Proxy n
         prox _ = Proxy
 
