1131d1350c2f49ba052a7d5016a6d1ff7887a704
Gabor Greif <ggreif@gmail.com>
2013-12-21 21:21:51 +0100

    WIP: trying to build directories

--- Data/SingTree.hs
+++ Data/SingTree.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE DataKinds, PolyKinds, KindSignatures, GADTs, TypeOperators,
              StandaloneDeriving, RankNTypes, ScopedTypeVariables,
-             ConstraintKinds, TypeFamilies #-}
+             ConstraintKinds, TypeFamilies, UndecidableInstances #-}
 
 module SingTree where
 
@@ -18,13 +18,37 @@ import Data.Thrist
 -- The tree must be parametrized in the list of all
 -- the paths it contains
 
+data List :: [[Symbol]] -> * where
+  Ni :: List '[]
+  Sn :: Apart ts t => List ts -> Tree' t -> List (ts `Append` t)
+
+type family Roots (as :: [[Symbol]]) :: [Symbol] where
+  Roots '[] = '[]
+  Roots ('[r] ': ps) = r ': Roots ps
+  Roots ((r ': rs) ': ps) = Roots (rs ': ps)
+
+type Apart (l :: [[Symbol]]) (r :: [[Symbol]]) = '[] ~ Intersect (Roots l) (Roots r)
+
+type family Intersect (l :: [k]) (r :: [k]) :: [k] where
+  Intersect '[] r = '[]
+  Intersect (l ': ls) (l ': rs) = l ': Intersect ls rs
+  --Intersect (l ': ls) (l ': rs) = l ': Intersect ls rs
+
+deriving instance Show (List t)
+
 data Tree' :: [[Symbol]] -> * where
-  Root' :: Tree' '[]
   File :: KnownSymbol file => Tree' '[ '[file] ]
-  --Dir :: 
+  Dir :: KnownSymbol dir => Proxy dir -> List many -> Tree' (many `AllAppend` dir)
+
+deriving instance Show (Tree' t)
+
+type family AllAppend (as :: [[Symbol]]) (b :: Symbol) :: [[Symbol]] where
+  AllAppend '[] b = '[]
+  AllAppend (p ': ps) b = Append p '[b] ': AllAppend ps b
 
 
 r0 = File :: Tree' '[ '["my.txt"] ]
+--r1 = Ni `Sn` r0 `Sn` r0
 
 data Tree :: [Symbol] -> * where
   Root :: Tree '[]
